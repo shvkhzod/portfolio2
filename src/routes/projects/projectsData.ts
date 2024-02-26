9061dc1c528bb6eaa9b8dff7dc4b0ef8edb64e05
Shakhzod Sharifov <thedigitaluzb@gmail.com>
2024-02-27 00:53:47 +0500

    Content update

--- src/routes/projects/projectsData.ts
+++ src/routes/projects/projectsData.ts
@@ -20,22 +20,22 @@ export const projectsData: ProjectData[] = [{
         {   image: "digitalThumb",     
         },
         {
-            paragraph: "I created very nice community consisting of 2000 people on Instagram",
+            paragraph: "I created very a community of programmers and designers consisting of 2000 people on Instagram",
 
    
         },
         {
             headline: "The reason",
-            paragraph: "I wanted to create a community where people can share their knowledge, experience and ideas. I wanted to create a platform where people can learn from each other and grow together. And I am very happy that I achieved it."
+            paragraph: "In 2021, I looked at Instagram and other resources but could not find any source that was producing good Uzbek tech content. During that period, we were still mainly indoors, so content was firing up in all networks. So I  wanted to create a community where people can share their knowledge, experience and ideas. I wanted to create a place where people can learn from each other and grow together. Plus, I wanted to consolidate my own knowledge by teaching others."
 
         },
         {
             headline: "The future",
-            paragraph: "I am planning to create a website and a youtube channel where I will share my knowledge and experience. I am also planning to create a platform where people can share their ideas and knowledge and grow together."
+            paragraph: "I am planning to create a website and a youtube channel where I will share exta knowledge and experience regarding to tech and design. I am also planning to create a special platform dedicated to learning skills related to technology and design. I am working on it these days, I think that it will be a great help for people who want to learn things on their own"
         },
         {
             headline: "What I learnt",
-            paragraph: "I discovered new people, became more confident about my communication skills"
+            paragraph: "I discovered new people, I got a chance to talk with experts and learn from them. Recieved lots of messages from total strangers how they were benefiting from the content I was creating, and boy, that gave different feeling"
         }
     ],
 },
@@ -65,8 +65,15 @@ export const projectsData: ProjectData[] = [{
             },
             {
                 headline: "Colors",
-                paragraph: "So we went with blue as primary color, as it gives a sense of technology and trust. For secondary color we chose white, to emphasize the primary color. And for accent colors, the different hues of blue and green were chosen to give a sense of calmness and growth. "
+                paragraph: "We went with blue as primary color, as it gives a sense of technology and trust. For secondary color we chose white, to emphasize the primary color. And for accent colors, the different hues of blue and green were chosen to give a sense of calmness and growth. ",
             },
+            {
+                image: "ever3",
+            },
+            {
+                headline: "The result",
+                paragraph: "The website was not launched yet, the development is currently in pause. But I completed the design and initial phase of development and presented ready project to their development team. I hope they will launch it soon and it will help them to grow and reach more people. "
+            }
         ]
     }
 ]
